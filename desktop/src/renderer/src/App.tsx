f381a734cdc3fb87825bb806da68cda557923b6c
Tisca Catalin <admin@tiscacatalin.com>
2026-02-03 15:43:26 +0200

    nice design

--- desktop/src/renderer/src/App.tsx
+++ desktop/src/renderer/src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom'
 import { SplashScreen } from './views/SplashScreen'
 import { WalletSelection } from './views/WalletSelection'
 import { WalletDashboard } from './views/WalletDashboard'
+import { Transfer } from './views/Transfer'
+import { Settings } from './views/Settings'
 
 function App(): React.JSX.Element {
   return (
@@ -9,6 +11,8 @@ function App(): React.JSX.Element {
       <Route path="/" element={<SplashScreen />} />
       <Route path="/wallets" element={<WalletSelection />} />
       <Route path="/dashboard" element={<WalletDashboard />} />
+      <Route path="/transfer" element={<Transfer />} />
+      <Route path="/settings" element={<Settings />} />
     </Routes>
   )
 }
